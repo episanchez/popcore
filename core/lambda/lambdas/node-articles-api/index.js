91f6ba0315af0a45fe08e0d92154731ed3ecd704
episanchez <charlie.quillard@epitech.eu>
2018-08-03 17:44:00 +0200

    update refresh lambda

--- core/lambda/lambdas/node-articles-api/index.js
+++ core/lambda/lambdas/node-articles-api/index.js
@@ -1,12 +1,49 @@
-'use strict';
+var mysql      = require('mysql');
+
+
+function get(cb){
+  var connection = mysql.createConnection({
+  host     : 'popvox-mysql-articles-db.cajp6pxatu6q.eu-west-3.rds.amazonaws.com',
+  user     : 'popvox_db_admin',
+  password : 'asvBJ{[#',
+  database : 'popvox_mysql_db_main'
+  });
+
+  var res = {};
+  connection.query("SELECT * FROM (SELECT * FROM article ORDER BY rand() LIMIT 10) T1  ORDER BY published_date LIMIT 1", function (error, results, fields) {
+    connection.end(function(errCon) //Don't do anything with end Error
+    {
+       // The connection is terminated now
+       cb(error, results[0]);
+    });
+  });
+  return res;
+}
+
 
 module.exports.handler = function(event, context, cb) {
-  cb(null, {
-    statusCode: 200,
-    headers: {},
-    body: JSON.stringify({
-      msg: 'This Lambda is not implemented yet',
-      event: event
-    })
-  });
+  var article = get(function (error, result){
+    if (error){
+      cb(null, {
+        statusCode: 404,
+        headers: {},
+        body: JSON.stringify({
+          msg: "article founded",
+          article_url: article,
+          event: event
+        })
+      })
+    }
+    else {
+      cb(null, {
+        statusCode: 200,
+        headers: {},
+        body: JSON.stringify({
+          msg: "article founded",
+          article_url: result.url,
+          event: event
+        })
+      })
+    }
+  })
 };
---
+++ infra/database_insert.sql
@@ -0,0 +1,26 @@
+
+-- INSERT MEDIA
+
+INSERT INTO media
+    (name,host_name,url_rss,is_indie)
+VALUES
+    ("7x7","7x7.press","https://www.7x7.press/feed.rss",true);
+
+-- INSERT 26 JULY
+INSERT INTO media
+    (name,host_name,url_rss,is_indie)
+VALUES
+    ("L'optimisme","loptimisme.com","https://www.loptimisme.com/feed",true),
+    ("Usbek & Rica","usbeketrica.com","https://usbeketrica.com/rss",true),
+    ("Reporterre","reporterre.net","https://reporterre.net/spip.php?page=backend-simple",true),
+    ("Reflets","reflets.info","https://reflets.info/feeds/public",true),
+    ("La releve et la peste","lareleveetlapeste.fr","https://lareleveetlapeste.fr/feed/",true),
+    ("Maddyness","maddyness.com","https://www.maddyness.com/feed/",true);
+
+
+-- INSERT 26 JULY
+INSERT INTO media
+    (name,host_name,url_rss,is_indie)
+VALUES
+    ("L'usine Nouvelle","usinenouvelle.com","https://www.usinenouvelle.com/rss/",false),
+    ("French Web","frenchweb.fr","https://www.frenchweb.fr/feed",false);
