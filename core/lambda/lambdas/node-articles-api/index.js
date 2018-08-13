84cf1d7201efdbeef23bb9e54823020efa4e9adf
episanchez <charlie.quillard@epitech.eu>
2018-08-13 22:59:16 +0200

    update api

--- core/lambda/lambdas/node-articles-api/index.js
+++ core/lambda/lambdas/node-articles-api/index.js
@@ -1,49 +1,47 @@
-var mysql      = require('mysql');
+var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+ host     : 'popvox-mysql-articles-db.cajp6pxatu6q.eu-west-3.rds.amazonaws.com',
+ user     : 'popvox_db_admin',
+ password : 'asvBJ{[#',
+ database : 'popvox_mysql_db_main'
+ });
 
 function get(cb){
-  var connection = mysql.createConnection({
-  host     : 'popvox-mysql-articles-db.cajp6pxatu6q.eu-west-3.rds.amazonaws.com',
-  user     : 'popvox_db_admin',
-  password : 'asvBJ{[#',
-  database : 'popvox_mysql_db_main'
-  });
-
-  var res = {};
-  connection.query("SELECT * FROM (SELECT * FROM article ORDER BY rand() LIMIT 10) T1  ORDER BY published_date LIMIT 1", function (error, results, fields) {
-    connection.end(function(errCon) //Don't do anything with end Error
-    {
-       // The connection is terminated now
-       cb(error, results[0]);
-    });
-  });
-  return res;
+ connection.query("SELECT * FROM (SELECT * FROM article ORDER BY rand() LIMIT 10) T1  ORDER BY published_date LIMIT 1", function (error, results, fields) {
+   connection.end(function(errCon) //Don't do anything with end Error
+   {
+      // The connection is terminated now
+      cb(error, results[0]);
+   });
+ });
 }
 
 
 module.exports.handler = function(event, context, cb) {
-  var article = get(function (error, result){
-    if (error){
-      cb(null, {
-        statusCode: 404,
-        headers: {},
-        body: JSON.stringify({
-          msg: "article founded",
-          article_url: article,
-          event: event
-        })
-      })
-    }
-    else {
-      cb(null, {
-        statusCode: 200,
-        headers: {},
-        body: JSON.stringify({
-          msg: "article founded",
-          article_url: result.url,
-          event: event
-        })
-      })
-    }
-  })
+ console.log(event)
+ var article = get(function (error, result){
+   if (error){
+     cb(null, {
+       statusCode: 404,
+       headers: {},
+       body: JSON.stringify({
+         msg: "article not founded",
+         error: error,
+         event: event
+       })
+     })
+   }
+   else {
+     cb(null, {
+       statusCode: 200,
+       headers: {},
+       body: JSON.stringify({
+         type: "text",
+         content: result.url,
+         event: event
+       })
+     })
+   }
+ })
 };
